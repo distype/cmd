056b80510261007dafec78c860d384553abb9cae
BR88C <br88c@br88c.dev>
2022-05-12 01:52:54 +0000

    build: TSC build for 259ea65547ab72fbb9e3eeb9d7871136e96b7a6d

--- dist/colors/DiscordColors.d.ts
+++ dist/colors/DiscordColors.d.ts
@@ -1,5 +1,6 @@
 /**
  * Discord colors.
+ * Since role colors do not have names, comprehensible aliases have been used as a substitute.
  */
 export declare const DiscordColors: {
     readonly BLURPLE: 5793266;
@@ -20,24 +21,84 @@ export declare const DiscordColors: {
     readonly LIGHT_MODE_BACKGROUND_PRIMARY: 16777215;
     readonly LIGHT_MODE_BACKGROUND_SECONDARY: 15922165;
     readonly LIGHT_MODE_BACKGROUND_TERTIARY: 14935528;
-    readonly ROLE_COLOR_0_0: 1752220;
-    readonly ROLE_COLOR_0_1: 3066993;
-    readonly ROLE_COLOR_0_2: 3447003;
-    readonly ROLE_COLOR_0_3: 10181046;
-    readonly ROLE_COLOR_0_4: 15277667;
-    readonly ROLE_COLOR_0_5: 15844367;
-    readonly ROLE_COLOR_0_6: 15105570;
-    readonly ROLE_COLOR_0_7: 15158332;
-    readonly ROLE_COLOR_0_8: 9807270;
-    readonly ROLE_COLOR_0_9: 6323595;
-    readonly ROLE_COLOR_1_0: 1146986;
-    readonly ROLE_COLOR_1_1: 2067276;
-    readonly ROLE_COLOR_1_2: 2123412;
-    readonly ROLE_COLOR_1_3: 7419530;
-    readonly ROLE_COLOR_1_4: 11342935;
-    readonly ROLE_COLOR_1_5: 12745742;
-    readonly ROLE_COLOR_1_6: 11027200;
-    readonly ROLE_COLOR_1_7: 10038562;
-    readonly ROLE_COLOR_1_8: 9936031;
-    readonly ROLE_COLOR_1_9: 5533306;
+    /**
+     * Row 1 Column 1.
+     */
+    readonly ROLE_SEA_GREEN: 1752220;
+    /**
+     * Row 1 Column 2.
+     */
+    readonly ROLE_GREEN: 3066993;
+    /**
+     * Row 1 Column 3.
+     */
+    readonly ROLE_BLUE: 3447003;
+    /**
+     * Row 1 Column 4.
+     */
+    readonly ROLE_PURPLE: 10181046;
+    /**
+     * Row 1 Column 5.
+     */
+    readonly ROLE_HOT_PINK: 15277667;
+    /**
+     * Row 1 Column 6.
+     */
+    readonly ROLE_YELLOW: 15844367;
+    /**
+     * Row 1 Column 7.
+     */
+    readonly ROLE_ORANGE: 15105570;
+    /**
+     * Row 1 Column 8.
+     */
+    readonly ROLE_CORAL: 15158332;
+    /**
+     * Row 1 Column 9.
+     */
+    readonly ROLE_GRAY: 9807270;
+    /**
+     * Row 1 Column 10.
+     */
+    readonly ROLE_DARK_GRAY: 6323595;
+    /**
+     * Row 2 Column 1.
+     */
+    readonly ROLE_DARK_SEA_GREEN: 1146986;
+    /**
+     * Row 2 Column 2.
+     */
+    readonly ROLE_DARK_GREEN: 2067276;
+    /**
+     * Row 2 Column 3.
+     */
+    readonly ROLE_DARK_BLUE: 2123412;
+    /**
+     * Row 2 Column 4.
+     */
+    readonly ROLE_DARK_PURPLE: 7419530;
+    /**
+     * Row 2 Column 5.
+     */
+    readonly ROLE_DARK_HOT_PINK: 11342935;
+    /**
+     * Row 2 Column 6.
+     */
+    readonly ROLE_DARK_YELLOW: 12745742;
+    /**
+     * Row 2 Column 7.
+     */
+    readonly ROLE_LIGHT_BROWN: 11027200;
+    /**
+     * Row 2 Column 8.
+     */
+    readonly ROLE_BROWN: 10038562;
+    /**
+     * Row 2 Column 9.
+     */
+    readonly ROLE_DULL_GRAY: 9936031;
+    /**
+     * Row 2 Column 10.
+     */
+    readonly ROLE_DULL_DARK_GRAY: 5533306;
 };
--- dist/colors/DiscordColors.js
+++ dist/colors/DiscordColors.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DiscordColors = void 0;
 /**
  * Discord colors.
+ * Since role colors do not have names, comprehensible aliases have been used as a substitute.
  */
 exports.DiscordColors = {
     BLURPLE: 0x5865F2,
@@ -23,24 +24,84 @@ exports.DiscordColors = {
     LIGHT_MODE_BACKGROUND_PRIMARY: 0xFFFFFF,
     LIGHT_MODE_BACKGROUND_SECONDARY: 0xF2F3F5,
     LIGHT_MODE_BACKGROUND_TERTIARY: 0xE3E5E8,
-    ROLE_COLOR_0_0: 0x1ABC9C,
-    ROLE_COLOR_0_1: 0x2ECC71,
-    ROLE_COLOR_0_2: 0x3498DB,
-    ROLE_COLOR_0_3: 0x9B59B6,
-    ROLE_COLOR_0_4: 0xE91E63,
-    ROLE_COLOR_0_5: 0xF1C40F,
-    ROLE_COLOR_0_6: 0xE67E22,
-    ROLE_COLOR_0_7: 0xE74C3C,
-    ROLE_COLOR_0_8: 0x95A5A6,
-    ROLE_COLOR_0_9: 0x607D8B,
-    ROLE_COLOR_1_0: 0x11806A,
-    ROLE_COLOR_1_1: 0x1F8B4C,
-    ROLE_COLOR_1_2: 0x206694,
-    ROLE_COLOR_1_3: 0x71368A,
-    ROLE_COLOR_1_4: 0xAD1457,
-    ROLE_COLOR_1_5: 0xC27C0E,
-    ROLE_COLOR_1_6: 0xA84300,
-    ROLE_COLOR_1_7: 0x992D22,
-    ROLE_COLOR_1_8: 0x979C9F,
-    ROLE_COLOR_1_9: 0x546E7A
+    /**
+     * Row 1 Column 1.
+     */
+    ROLE_SEA_GREEN: 0x1ABC9C,
+    /**
+     * Row 1 Column 2.
+     */
+    ROLE_GREEN: 0x2ECC71,
+    /**
+     * Row 1 Column 3.
+     */
+    ROLE_BLUE: 0x3498DB,
+    /**
+     * Row 1 Column 4.
+     */
+    ROLE_PURPLE: 0x9B59B6,
+    /**
+     * Row 1 Column 5.
+     */
+    ROLE_HOT_PINK: 0xE91E63,
+    /**
+     * Row 1 Column 6.
+     */
+    ROLE_YELLOW: 0xF1C40F,
+    /**
+     * Row 1 Column 7.
+     */
+    ROLE_ORANGE: 0xE67E22,
+    /**
+     * Row 1 Column 8.
+     */
+    ROLE_CORAL: 0xE74C3C,
+    /**
+     * Row 1 Column 9.
+     */
+    ROLE_GRAY: 0x95A5A6,
+    /**
+     * Row 1 Column 10.
+     */
+    ROLE_DARK_GRAY: 0x607D8B,
+    /**
+     * Row 2 Column 1.
+     */
+    ROLE_DARK_SEA_GREEN: 0x11806A,
+    /**
+     * Row 2 Column 2.
+     */
+    ROLE_DARK_GREEN: 0x1F8B4C,
+    /**
+     * Row 2 Column 3.
+     */
+    ROLE_DARK_BLUE: 0x206694,
+    /**
+     * Row 2 Column 4.
+     */
+    ROLE_DARK_PURPLE: 0x71368A,
+    /**
+     * Row 2 Column 5.
+     */
+    ROLE_DARK_HOT_PINK: 0xAD1457,
+    /**
+     * Row 2 Column 6.
+     */
+    ROLE_DARK_YELLOW: 0xC27C0E,
+    /**
+     * Row 2 Column 7.
+     */
+    ROLE_LIGHT_BROWN: 0xA84300,
+    /**
+     * Row 2 Column 8.
+     */
+    ROLE_BROWN: 0x992D22,
+    /**
+     * Row 2 Column 9.
+     */
+    ROLE_DULL_GRAY: 0x979C9F,
+    /**
+     * Row 2 Column 10.
+     */
+    ROLE_DULL_DARK_GRAY: 0x546E7A
 };
