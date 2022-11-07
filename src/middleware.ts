c2f0882c8915e0dac6c19220628fff00883d7021
BR88C <br88c@br88c.dev>
2022-11-07 18:49:13 -0500

    Use interface for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,4 +2,4 @@
  * Metadata for middleware.
  * Modify this type to easily create typed data for middleware.
  */
-export type MiddlewareMeta = never;
+export interface MiddlewareMeta {} // eslint-disable-line @typescript-eslint/no-empty-interface
